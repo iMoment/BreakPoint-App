7bc92f062c127cf3ce762bb78b79c523fc7cd7d7
Stanley Pan <stanleypan.egre@gmail.com>
2017-09-22 03:16:55 +0900

    AuthVC, LoginVC, InsetTextField, ShadowView swift file creation.

---
+++ BreakPoint/View/InsetTextField.swift
@@ -0,0 +1,21 @@
+//
+//  InsetTextField.swift
+//  BreakPoint
+//
+//  Created by Stanley Pan on 22/09/2017.
+//  Copyright © 2017 Stanley Pan. All rights reserved.
+//
+
+import UIKit
+
+class InsetTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
