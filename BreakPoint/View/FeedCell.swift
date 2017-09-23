34c3e92349d68bd527d620619b0acb15fbfb68e5
Stanley Pan <stanleypan.egre@gmail.com>
2017-09-23 22:29:51 +0900

    Create FeedCell

---
+++ BreakPoint/View/FeedCell.swift
@@ -0,0 +1,16 @@
+//
+//  FeedCell.swift
+//  BreakPoint
+//
+//  Created by Stanley Pan on 23/09/2017.
+//  Copyright © 2017 Stanley Pan. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+    
+    //  MARK: Outlets
+    
+    
+}
