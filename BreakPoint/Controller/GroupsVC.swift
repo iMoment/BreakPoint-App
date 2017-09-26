805a96222d87c9b2008e49c014d39dcee192d75e
Stanley Pan <stanleypan.egre@gmail.com>
2017-09-27 00:17:17 +0900

    Create Group model

--- BreakPoint/Controller/GroupsVC.swift
+++ BreakPoint/Controller/GroupsVC.swift
@@ -12,11 +12,24 @@ class GroupsVC: UIViewController {
     
     //  MARK: Outlets
     @IBOutlet weak var groupsTableView: UITableView!
+    
+    //  MARK: Variables
+    var groupsArray = [Group]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         groupsTableView.dataSource = self
         groupsTableView.delegate = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        DataService.instance.REF_GROUPS.observe(.value) { (snapshot) in
+            DataService.instance.getAllGroups { (returnedGroupsArray) in
+                self.groupsArray = returnedGroupsArray
+                self.groupsTableView.reloadData()
+            }
+        }
     }
 }
 
@@ -26,12 +39,13 @@ extension GroupsVC: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return groupsArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "groupCell", for: indexPath) as? GroupCell else { return UITableViewCell() }
-        cell.configureCell(title: "Hello", description: "Hello World", memberCount: 5)
+        let group = groupsArray[indexPath.row]
+        cell.configureCell(title: group.groupTitle, description: group.groupDescription, memberCount: group.memberCount)
         return cell
     }
 }
